1a52922475a3e44cffc93ce1a1d42925eb1903a0
Phillips <victor.m.phillips2.ctr@us.navy.mil>
2024-05-04 13:48:30 -0500

    pushing victors changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './features/card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Style Guide';
-  content = 'Custom Components Go Here...'
+  public title = 'Style Guide';
+  public content = 'Custom Components Go Here...'
+
+  public emission = false;
+
+  public toggleEmission()
+  {
+    this.emission = !this.emission;
+  }
 }
