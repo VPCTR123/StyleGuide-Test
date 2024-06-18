eb41c5ce867c36ebabc13e216be8af8b0229ebf4
Victor <vmattphillips1997@gmail.com>
2024-06-18 15:08:20 -0400

    Huge restructure pls no break

--- StyleGuide/BOL Design System/js/toggles/toggleActive.js
+++
@@ -1,5 +0,0 @@
-
-function toggleActive(passedId) {
-    const target = document.getElementById('target-' + passedId);
-    target.classList.toggle("active");
-}
--- StyleGuide/BOL Design System/js/toggles/toggleDarkMode.js
+++
@@ -1,5 +0,0 @@
-function toggleDarkMode() {
-    var body = document.querySelector('body');
-    // Toggle between adding/removing the 'theme-dark' class
-    body.classList.toggle('theme-dark');
-  }
