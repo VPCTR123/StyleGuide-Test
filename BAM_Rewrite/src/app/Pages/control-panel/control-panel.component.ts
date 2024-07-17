e531d4cfe2d7919752184e2a9dd54028a248da1f
Victor <vmattphillips1997@gmail.com>
2024-07-17 14:04:57 -0400

    fixed control pannel

--- BAM_Rewrite/src/app/Pages/control-panel/control-panel.component.ts
+++ BAM_Rewrite/src/app/Pages/control-panel/control-panel.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../Features/header/header.component';
 
 @Component({
   selector: 'app-control-panel',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './control-panel.component.html',
   styleUrl: './control-panel.component.scss'
 })
