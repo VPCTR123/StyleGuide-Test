b15ae3f45ec845efc32962d04f8ce7ea0f6cdb5c
murrayrv <richard.v.murray@saic.com>
2024-07-18 11:10:34 -0500

    7.18.24

--- BOL Design System/js/toggles/togglePopup.js
+++ BOL Design System/js/toggles/togglePopup.js
@@ -36,8 +36,8 @@ function togglePopup(event, popupId) {
 
 document.addEventListener('click', function (event) {
 
-    if (activePopup){
-        const outsideClick =  !activePopup.contains(event.target);
+    if (activePopup) {
+        const outsideClick = !activePopup.contains(event.target);
 
         if (outsideClick && activePopup) {
             activePopup.classList.remove('visible');
@@ -50,12 +50,12 @@ document.addEventListener('click', function (event) {
             activeClose = null;
         }
     }
-    
- 
+
+
 });
 document.addEventListener('click', function (event) {
 
-    if (activePopup && activeClose){
+    if (activePopup && activeClose) {
         const closeClick = activeClose.contains(event.target);
 
         if (closeClick && activePopup) {
@@ -68,18 +68,45 @@ document.addEventListener('click', function (event) {
             activeClose = null;
         }
     }
-    
- 
+
+
 });
 
 
 // || event.target.closest('.overlay')   // if (closeClick && activePopup) {
-    //     console.log("closeclick");
-    //     activePopup.classList.remove('visible');
-    //     if (activeOverlay) {
-    //         activeOverlay.classList.remove('visible');
-    //     }
-    //     console.log("test");
-    //     activePopup = null;
-    //     activeOverlay = null;
-    // }
+//     console.log("closeclick");
+//     activePopup.classList.remove('visible');
+//     if (activeOverlay) {
+//         activeOverlay.classList.remove('visible');
+//     }
+//     console.log("test");
+//     activePopup = null;
+//     activeOverlay = null;
+// }
+
+
+//FOCUS FLIP FLOP
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all elements with the class 'focus-flipflop'
+    const elements = document.querySelectorAll('.focus-flipflop');
+  
+    // Add click event listener to each element
+    elements.forEach(element => {
+      element.addEventListener('click', function () {
+        // Check if the element currently has focus
+        const hasFocus = document.activeElement === this;
+  
+        // If the element has focus, remove focus programmatically
+        if (hasFocus) {
+          this.blur(); // Remove focus
+        } else {
+          this.focus(); // Set focus
+        }
+      });
+  
+      // Prevent default behavior for better simulation
+      element.addEventListener('mousedown', function (event) {
+        event.preventDefault();
+      });
+    });
+  });
